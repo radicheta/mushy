c6773009c249283770924c1e08a805bd9e5ce1e4
radicheta <radicheta@gmail.com>
2025-01-30 12:38:46 -0300

    wip ros2 iron port

--- src/life_support/ls_core/setup.py
+++ src/life_support/ls_core/setup.py
@@ -4,22 +4,20 @@ package_name = 'ls_core'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='Life support base functionality for mushroom farm',
+    maintainer='santi',
+    maintainer_email='todo@todo.com',
+    description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
