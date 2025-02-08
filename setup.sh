02e0d70ebea49c340198da800dc748ded211aeb8
radicheta <radicheta@gmail.com>
2025-02-08 16:03:08 -0300

    dockerizing jazzy

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Add Gazebo Fortress installation to PATH
-export PATH=$HOME/robotics/gz_ws/install/bin:~/ros2_iron_ws/install/bin:$PATH
+# # Add Gazebo Fortress installation to PATH
+# export PATH=$HOME/robotics/gz_ws/install/bin:~/ros2_iron_ws/install/bin:$PATH
 
-# Set Ignition configuration path
-export IGN_CONFIG_PATH="/usr/local/share/ignition:$HOME/robotics/gz_ws/install/share/ignition"
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/robotics/gz_ws/install/lib/ign-gazebo-6/plugins:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
+# # Set Ignition configuration path
+# export IGN_CONFIG_PATH="/usr/local/share/ignition:$HOME/robotics/gz_ws/install/share/ignition"
+# export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/robotics/gz_ws/install/lib/ign-gazebo-6/plugins:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH
 
 # Optional? Source the ROS and Gazebo setup scripts
 # source ~/ros2_iron_ws/install/setup.sh
 # source ~/robotics/gz_ws/install/setup.sh
 
 
-source ~/mushroom_farm_ws/install/setup.sh
+# source ~/mushroom_farm_ws/install/setup.sh
